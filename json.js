f7709b8796ec8ea93d3c5fb65fdd8837afc186b0
Alexandr <algran83@mail.ru>
2023-01-30 01:26:20 +0400

    edit

---
+++ json.js
@@ -0,0 +1,28 @@
+const dataProducts = `
+[
+    {
+        "img": "https://cdn1.flamp.ru/cea91ebb1612408cebc92a1792f07cb8.png",
+        "name": "MANGO  PEOPLE  T-SHIRT",
+        "Price": "$300",
+        "Color": "Red",
+        "Size": "Xl",
+        "Quantity": 2
+    },
+    {
+        "img": "https://webmentor.gr/wp-content/uploads/2018/11/logo300X300.png",
+        "name": "ELLERY X M'O CAPSULE",
+        "Price": "$52",
+        "Color": "Black",
+        "Size": "Xl",
+        "Quantity": 1
+    },
+    {
+        "img": "/17.jpg",
+        "name": "CAPSULE",
+        "Price": "$502",
+        "Color": "Bl",
+        "Size": "X",
+        "Quantity": 11
+    }
+]
+`;
